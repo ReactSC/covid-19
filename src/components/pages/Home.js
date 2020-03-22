e2b7de291b0e51e7852bdd83321b029e9065d89b
Sofiqul Islam <schoolwab@gmail.com>
2020-03-22 18:19:02 +0600

    resolve API Settings

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
   const md = useMediaQuery(theme.breakpoints.up("md"));
   const lg = useMediaQuery(theme.breakpoints.up("lg"));
 
-  const { stats, loading, error } = useStats("https://covid19.mathdro.id/api");
+  const { stats, loading, error } = useStats("https://covid19.mathdro.id/api/countries/BD/confirmed");
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error...</p>;
   return (
@@ -20,35 +20,48 @@ const Home = () => {
       <Grid container spacing={3}>
         {lg ? (
           <Lg
-            confirmed={stats.confirmed.value}
-            Recovered={stats.recovered.value}
-            Deaths={stats.deaths.value}
+            confirmed={stats[0].confirmed}
+            Recovered={stats[0].recovered}
+            Deaths={stats[0].deaths}
           />
         ) : md ? (
           <Md
-            confirmed={stats.confirmed.value}
-            Recovered={stats.recovered.value}
-            Deaths={stats.deaths.value}
+            confirmed={stats[0].confirmed}
+            Recovered={stats[0].recovered}
+            Deaths={stats[0].deaths}
           />
         ) : sm ? (
           <Sm
-            confirmed={stats.confirmed.value}
-            Recovered={stats.recovered.value}
-            Deaths={stats.deaths.value}
+            confirmed={stats[0].confirmed}
+            Recovered={stats[0].recovered}
+            Deaths={stats[0].deaths}
           />
         ) : xs ? (
           <Xs
-            confirmed={stats.confirmed.value}
-            Recovered={stats.recovered.value}
-            Deaths={stats.deaths.value}
+            confirmed={stats[0].confirmed}
+            Recovered={stats[0].recovered}
+            Deaths={stats[0].deaths}
           />
         ) : (
           xs
         )}
       </Grid>
-      <LastUpdateTime time={ stats.lastUpdate } />
+      <Total />
+      <LastUpdateTime time={stats[0].lastUpdate } />
       <SocialLinks />
     </Container>
   );
 }
 export default Home;
+
+const Total = () => {
+  const { stats, loading, error } = useStats("https://covid19.mathdro.id/api/");
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error...</p>;
+
+  return (
+    <div className="p-2 bg-dark">
+      <p className="text-light mt-3">Confirmed: {stats.confirmed.value} | Recovered: {stats.recovered.value} | Deaths: {stats.deaths.value}</p>
+    </div>
+  )
+}
